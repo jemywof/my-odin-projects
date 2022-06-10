c8a0e08ac690fdab9b28c0fb5423f00689eef511
Jeremy Wolfe <jeremy.t.wolfe@outlook.com>
2022-06-10 16:25:59 -0400

    Finished bare functions for the UI exercise! Ugly but functional.

--- full-stack-javascript/dynamic-UI/script.js
+++ full-stack-javascript/dynamic-UI/script.js
@@ -19,11 +19,14 @@ const DropDownMenu = (() => {
     const dropList = document.createElement('div')
     dropList.classList.add('drop-down-list')
 
+    const dropContent = document.createElement('div')
+    dropContent.classList.add('drop-down-content')
+    dropList.appendChild(dropContent)
+
     const addOption = function (name) {
-      const newOption = document.createElement('a')
-      newOption.classList.add('drop-down-item')
+      const newOption = document.createElement('p')
       newOption.textContent = name
-      dropList.appendChild(newOption)
+      dropContent.appendChild(newOption)
     }
 
     addOption('Option 1')
@@ -38,40 +41,6 @@ const DropDownMenu = (() => {
     header.appendChild(mainDiv)
     DropDownMenu.isLoaded = true
   }
-
-  /*const toggleDropDown = function (dropDiv) {
-    if (DropDownMenu.isDropped) {
-      items = document.querySelectorAll('.drop-down-item')
-      items.forEach(item => {
-        item.remove()
-      })
-      DropDownMenu.isDropped = false
-      return
-    }
-    if (!dropDiv) {
-      console.error('Dropdown menu has not been loaded. Sorry!')
-      return
-    }
-
-    const dropList = document.createElement('div')
-    dropList.classList.add('drop-list')
-    const addOption = function (name) {
-      const newOption = document.createElement('a')
-      newOption.classList.add('drop-down-item')
-      newOption.textContent = name
-      dropList.appendChild(newOption)
-    }
-
-    addOption('Option 1')
-    addOption('Option 2')
-    addOption('Option 3')
-    addOption('Option 4')
-
-    dropDiv.appendChild(dropList)
-
-    DropDownMenu.isDropped = true
-  }
-  */
 
   return {
     toggle
@@ -95,7 +64,7 @@ const MobileMenu = (() => {
       mobileButton.textContent = 'BUTTON?'
       mobileButton.classList.add('mobile-button')
       mobileButton.onclick = function() {
-        this.textContent = 'I DO NOTHING LOL'
+        this.textContent = 'I DO NOTHING'
       }
       mainDiv.appendChild(mobileButton)
     }
@@ -111,6 +80,8 @@ const MobileMenu = (() => {
 
 const ImageSlider = (() => {
   let isLoaded = false
+  const IMAGECOUNT = 3
+  let activeImageIndex = 0
   const toggle = function () {
     if (ImageSlider.isLoaded) {
       const mainDiv = document.querySelector('.image-slider-div')
@@ -129,23 +100,94 @@ const ImageSlider = (() => {
     longImageBox.classList.add('long-image-box')
     imageFrame.appendChild(longImageBox)
 
-    const insertImage = function (source, alt) {
+    const insertImage = function (source, id, alt) {
       const image = document.createElement('img')
       image.classList.add('slider-image')
       image.setAttribute('src', source)
+      image.id = `slider${id}`;
       if (alt) {
         image.setAttribute('alt', alt)
       }
       imageFrame.appendChild(image)
     }
 
-    insertImage('images/first.jpg', 'garfield as art')
-    insertImage('images/second.jpg', 'a garfield plushy in a car')
-    insertImage('images/third.jpg', 'a cat that almost looks like garfield')
+    insertImage('images/first.jpg', 0, 'garfield as art')
+    insertImage('images/second.jpg', 1, 'a garfield plushy in a car')
+    insertImage('images/third.jpg', 2, 'a cat that almost looks like garfield')
+
+    const navContainer = document.createElement('div')
+    navContainer.classList.add('nav-container')
+    mainDiv.appendChild(navContainer)
+
+    //createArrow: adds the arrows to the UI
+    //also holds the arrows' 'slide' functionality (bad design oops)
+    const createArrow = function (src, direction) {
+      const arrow = document.createElement('img')
+      arrow.classList.add('nav-arrow')
+      arrow.setAttribute('src', src)
+      arrow.onclick = function () {
+        const previouslyActiveDot = document.getElementById(`arrow${activeImageIndex}`)
+        if (previouslyActiveDot == null) {
+          console.error(`previouslyActiveDot is set to ${previouslyActiveDot}`)
+          return
+        }
+
+        const oldImage = document.getElementById(`slider${activeImageIndex}`)
+
+        activeImageIndex += direction
+        if (activeImageIndex < 0) {
+          activeImageIndex = 0
+          return
+        }
+        if (activeImageIndex > IMAGECOUNT - 1) {
+          activeImageIndex = IMAGECOUNT - 1
+          return
+        }
+
+        const activeDot = document.getElementById(`arrow${activeImageIndex}`)
+        if (activeDot == null) {
+          console.error(`activeDot is set to ${activeDot}`)
+          return
+        }
+        const newImage = document.getElementById(`slider${activeImageIndex}`)
+
+        previouslyActiveDot.classList.remove('dot-active')
+        oldImage.classList.remove('active')
+        activeDot.classList.add('dot-active')
+        newImage.classList.add('active')
+
+      }
+      navContainer.appendChild(arrow)
+    }
+
+    const leftDirection = -1
+    const rightDirection = 1
+
+    const createDot = function (number) {
+      const dot = document.createElement('div')
+      dot.classList.add('image-dot')
+      dot.id = `arrow${number}`
+      navContainer.appendChild(dot)
+      if (number === 0) {
+        dot.classList.add('dot-active')
+      }
+    }
+
+    createArrow('images/left-arrow.png', leftDirection)
+
+    for (let i = 0; i < IMAGECOUNT; i++) {
+      createDot(i)
+    }
+    
+    createArrow('images/right-arrow.png', rightDirection)
 
     const buttonList = document.querySelector('.button-list')
     buttonList.appendChild(mainDiv)
     ImageSlider.isLoaded = true
+
+    //make the first image visible (bad design again oops)
+    document.getElementById(`slider0`).classList.add('active')
+
   }
   return {
     toggle,
